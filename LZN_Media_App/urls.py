0587858afbb07fea0ce03d6cda65dada52baae43
Christiaan Quyn <christianq010@gmail.com>
2017-07-07 18:13:03 +0530

    feat: change static site to be base url of main app

--- LZN_Media_App/urls.py
+++ LZN_Media_App/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^static_site/', include('static_site.urls')),
+    url(r'^$', include('static_site.urls')),
 ]
